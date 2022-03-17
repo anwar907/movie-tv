4f3c9752d2e1817bf1a20d34f7e6106050a46b83
anwar901 <anwar.sap@foxlogger.info>
2022-03-17 17:43:36 +0700

    fix navigator push repleshment in splash screen

--- lib/presentation/navigator.dart
+++ lib/presentation/navigator.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 Future replacePage(BuildContext context, Widget page) async {
-  return await Navigator.push(context, MaterialPageRoute(builder: (_) => page));
+  return await Navigator.pushReplacement(
+      context, MaterialPageRoute(builder: (_) => page));
 }
 
 Future pushPage(BuildContext context, Widget page) async {
